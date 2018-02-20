28f1c67a85a03184aab924722d8aedd6883e50f9
Dan Manning <dmanning23@hotmail.com>
2018-02-20 15:33:16 -0600

    moved to windowsdx

--- RectangleF/RectangleF.WindowsDX/Properties/AssemblyInfo.cs
+++ RectangleF/RectangleF.WindowsDX/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RectangleF.DesktopGL")]
+[assembly: AssemblyTitle("RectangleF.WindowsDX")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RectangleF.DesktopGL")]
+[assembly: AssemblyProduct("RectangleF.WindowsDX")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
